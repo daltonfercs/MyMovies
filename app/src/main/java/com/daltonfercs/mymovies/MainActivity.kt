318132269f160dc2b25e9e586017ac7a5313bf76
Dalton Celadita <dafecesa.dfcs@gmail.com>
2021-05-15 20:31:55 -0500

    Importando binding

--- app/src/main/java/com/daltonfercs/mymovies/MainActivity.kt
+++ app/src/main/java/com/daltonfercs/mymovies/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.daltonfercs.mymovies
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.daltonfercs.mymovies.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.message.text    =   "Hello Dalton..."
     }
 }

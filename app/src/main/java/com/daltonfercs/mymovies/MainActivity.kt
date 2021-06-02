81c8a296c2770e36ad067903cc5113a1a77e2002
Dalton Celadita <dafecesa.dfcs@gmail.com>
2021-06-02 16:04:37 -0500

    probando vistas

--- app/src/main/java/com/daltonfercs/mymovies/MainActivity.kt
+++ app/src/main/java/com/daltonfercs/mymovies/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 import com.daltonfercs.mymovies.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +14,11 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
-        binding.message.text    =   "Hello Dalton..."
+        binding.showButton.setOnClickListener{
+            val message = binding.messege.text
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+        }
+
+
     }
 }
